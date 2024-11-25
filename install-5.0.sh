c80213b594746663c5b234614100befcf5ddd332
root <root@susemanager.weiss.ddnss.de>
2024-11-25 10:57:39 +0100

    adding install script for susemanager 5

---
+++ install-5.0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+mgrctl cp ./usr/share/susemanager/salt/images/kiwi-image-build.sls server:/usr/share/susemanager/salt/images/kiwi-image-build.sls
+mgrctl cp ./usr/share/susemanager/salt/services/kiwi-image-server.sls server:/usr/share/susemanager/salt/services/kiwi-image-server.sls
